0e3bd1c0018f65bfc978fabba3db33633f982667
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-21 11:21:49 +0530

    lol3

---
+++ Four.java
@@ -0,0 +1,16 @@
+class Four{
+
+	public static void main(String ma[]){
+		float sgpa[] = {5.5f,5.6f,5.7f,5.8f,5.9f,6.0f,6.01f,6.89f,6.92f,6.99f};
+		System.out.println(sgpa[0]);
+		System.out.println(sgpa[1]);
+		System.out.println(sgpa[2]);
+		System.out.println(sgpa[3]);
+		System.out.println(sgpa[4]);
+		System.out.println(sgpa[5]);
+		System.out.println(sgpa[6]);
+		System.out.println(sgpa[7]);
+		System.out.println(sgpa[8]);
+		System.out.println(sgpa[9]);
+	}
+}
